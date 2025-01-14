2a82859dfb6aabe8200c8c429d62f34a34c5c9d5
Chetan Shinde <officialchetan15@gmail.com>
2025-01-15 02:09:07 +0530

    Update 5.sql

--- Query Files/5.sql
+++ Query Files/5.sql
@@ -17,3 +17,24 @@ FROM pizza_sales
 GROUP BY pizza_name
 ORDER BY Total_Revenue asc
 limit 5;
+
+
+-- Top 5 Pizzas by Quantity
+
+SELECT  pizza_name, SUM(quantity) AS Total_Pizza_Sold
+FROM pizza_sales
+GROUP BY pizza_name
+ORDER BY Total_Pizza_Sold DESC
+limit 5;
+
+
+
+
+-- Bottom 5 Pizzas by Quantity
+
+
+SELECT  pizza_name, SUM(quantity) AS Total_Pizza_Sold
+FROM pizza_sales
+GROUP BY pizza_name
+ORDER BY Total_Pizza_Sold asc
+limit 5;
