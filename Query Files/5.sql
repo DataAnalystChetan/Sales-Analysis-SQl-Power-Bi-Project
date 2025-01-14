8473ab8dc55795fc832f95b76d7b38c8a572c0c1
Chetan Shinde <officialchetan15@gmail.com>
2025-01-15 02:09:34 +0530

    Update 5.sql

--- Query Files/5.sql
+++ Query Files/5.sql
@@ -38,3 +38,23 @@ FROM pizza_sales
 GROUP BY pizza_name
 ORDER BY Total_Pizza_Sold asc
 limit 5;
+
+
+
+
+
+-- Top 5 Pizzas by Total Orders
+
+SELECT  pizza_name, COUNT(DISTINCT order_id) AS Total_Orders
+FROM pizza_sales
+GROUP BY pizza_name
+ORDER BY Total_Orders DESC
+limit 5;
+
+--  Top 5 Pizzas by Total Orders
+
+SELECT  pizza_name, COUNT(DISTINCT order_id) AS Total_Orders
+FROM pizza_sales
+GROUP BY pizza_name
+ORDER BY Total_Orders asc
+limit 5;
